e2d5466f01e90cc67b8d7dfbc395be96e9cb85f4
Juju-archy <julie.pouny@clever-cloud.com>
2024-09-09 21:49:07 +0200

    New config

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'instanceid' => 'ocyp0eyavzdr',
+  'passwordsalt' => 'ofwdpeZ+aI5a2yblqbJ9LGzfumUqnS',
+  'secret' => 'coExnpRwgd7Loh9Mp2WE/oSbOkWkJrngpWq1GokJllHFuygr',
+  'trusted_domains' => 
+  array (
+    0 => 'app-368a6fdf-e902-4e34-abea-a17e0d462556.cleverapps.io',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'pgsql',
+  'version' => '29.0.6.1',
+  'overwrite.cli.url' => 'http://app-368a6fdf-e902-4e34-abea-a17e0d462556.cleverapps.io',
+  'dbname' => 'bl6rjpha6c7zlcyrupbk',
+  'dbhost' => 'bl6rjpha6c7zlcyrupbk-postgresql.services.clever-cloud.com:5820',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'u5qi3wqsy4981umg3nph',
+  'dbpassword' => 'M1jorE8zOoUeezR9ZG93',
+  'installed' => true,
+  'defaultapp' => '',
+);
+
