d98a187558abc27533953d5d51e270e74780c98d
Juju-archy <julie.pouny@clever-cloud.com>
2024-09-10 17:45:55 +0200

    PHP modif via docker

---
+++ root/.bash_history
@@ -0,0 +1,12 @@
+ls
+cd config/
+ls
+cat config.sample.php 
+ls
+cd ..
+ls
+ls
+cd config/
+ls
+cd ..
+vim config/config.php 
---
+++ usr/local/include/php/ext/gd/config.h
@@ -0,0 +1,124 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Whether to build gd as dynamic module */
+#define COMPILE_DL_GD 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `fabsf' function. */
+/* #undef HAVE_FABSF */
+
+/* Define to 1 if you have the `floorf' function. */
+/* #undef HAVE_FLOORF */
+
+/* Does gdImageCreateFromAvif work? */
+/* #undef HAVE_GD_AVIF */
+
+/* Does gdImageCreateFromBmp work? */
+#define HAVE_GD_BMP 1
+
+/* */
+#define HAVE_GD_BUNDLED 1
+
+/* */
+#define HAVE_GD_FREETYPE 1
+
+/* */
+/* #undef HAVE_GD_GET_INTERPOLATION */
+
+/* Does gdImageCreateFromJpeg work? */
+#define HAVE_GD_JPG 1
+
+/* */
+/* #undef HAVE_GD_LIBVERSION */
+
+/* Does gdImageCreateFromPng work? */
+#define HAVE_GD_PNG 1
+
+/* Does gdImageCreateFromTga work? */
+#define HAVE_GD_TGA 1
+
+/* Does gdImageCreateFromWebp work? */
+#define HAVE_GD_WEBP 1
+
+/* Does gdImageCreateFromXpm work? */
+/* #undef HAVE_GD_XPM */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* */
+/* #undef HAVE_LIBAVIF */
+
+/* */
+#define HAVE_LIBFREETYPE 1
+
+/* */
+/* #undef HAVE_LIBGD */
+
+/* */
+#define HAVE_LIBJPEG 1
+
+/* */
+#define HAVE_LIBPNG 1
+
+/* */
+#define HAVE_LIBWEBP 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* */
+/* #undef HAVE_XPM */
+
+/* */
+/* #undef JISX0208 */
+
+/* Define to the address where bug reports for this package should be sent. */
+/* #undef PACKAGE_BUGREPORT */
+
+/* Define to the full name of this package. */
+/* #undef PACKAGE_NAME */
+
+/* Define to the full name and version of this package. */
+/* #undef PACKAGE_STRING */
+
+/* Define to the one symbol short name of this package. */
+/* #undef PACKAGE_TARNAME */
+
+/* Define to the home page for this package. */
+/* #undef PACKAGE_URL */
+
+/* Define to the version of this package. */
+/* #undef PACKAGE_VERSION */
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
+#define STDC_HEADERS 1
+
+/* */
+/* #undef USE_GD_JISX0208 */
